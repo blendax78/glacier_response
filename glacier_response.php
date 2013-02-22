d7808710427cf50430983f6c589c3a4d229d2e85
Eric Vintimilla <evintimi@evintimi-mac.local>
2013-02-22 16:41:44 -0500

    Initial Import

---
+++ glacier_response.php
@@ -0,0 +1,60 @@
+#!/usr/bin/php
+<?php
+
+/*
+Script Requires MeekroDB: http://www.meekro.com/ and amazon-glacier-cmd-interface https://github.com/uskudnik/amazon-glacier-cmd-interface
+
+Example:
+
+glacier_response.php <JSON glacier-cmd response> <vault name>
+
+php glacier_response.php "$(glacier-cmd upload --name 'bookmarks.html' --description 'bookmarks.html' Test_Vault bookmarks.html)" Test_Vault
+
+Using find:
+find . -name 'find*' -exec bash -c 'abc=$(glacier-cmd upload --name "{}" --description "{}" Test_Vault "{}") ; php /home/blendax/Saves/scripts/php/glacier_response.php "$abc" Test_Vault' \;
+
+*/
+
+
+require("meekrodb.2.1.class.php");
+DB::$host = "";
+DB::$user = "";
+DB::$password = "";
+DB::$dbName = "";
+
+process_input($argv,$argc);
+
+function process_input($args = null, $argc = 0){
+  if ($argc == 1){
+		exit;
+	}else{
+		$response = $args[1];
+		$vault = $args[2];
+	}
+	#Turn JSON into an array
+	$json = json_decode(strstr($response,"{"),true);
+	process($json["Created archive with ID"],$json["Uploaded file"],$json["Archive SHA256 tree hash"], $vault);
+}
+
+function process($id, $name, $hash, $vault = "Test_Vault"){
+	$result = null;
+	$result = DB::query("select * from glacier where filename = %s",
+		$name);
+	$output = array();
+	if (count($result) > 0){
+		foreach($result as $r){
+			#Delete from glacier
+			exec("/usr/local/bin/glacier-cmd rmarchive $vault '{$r['archive_id']}'",$output);
+			DB::query("update glacier set updated = now() where id = {$r['id']};");
+		}
+	}else{
+		DB::query("insert into glacier (archive_id,hash,filename,created, updated) values ( %s, %s, %s, now(), now() );",
+			$id,
+			$hash,
+			$name);
+
+	}
+#var_dump($output);
+}
+
+?>
---
+++ glacier_tbl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `glacier` (  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `archive_id` varchar(255) DEFAULT NULL,
+	`hash` varchar(255) DEFAULT NULL,
+	`filename` varchar(1000) DEFAULT NULL,
+	`created` datetime DEFAULT NULL,
+	`updated` datetime DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
